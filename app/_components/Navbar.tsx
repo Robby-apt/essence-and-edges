0d1272d70c9a2f97173a2d85bf9b2eb0021e0dec
Robby <robin.kw99@yahoo.com>
2025-12-22 16:20:58 +0300

    Responsive setup

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,7 +1,30 @@
+'use client';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
 export default function Navbar() {
-    return (
-        <nav>
-            <h1 className="title">ESSENCE & EDGES</h1>
-        </nav>
-    );
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	return (
+		<nav>
+			<div className="responsiveMenu navIcon">
+				{isMenuOpen ? (
+					<FontAwesomeIcon
+						icon={faXmark}
+						onClick={() => setIsMenuOpen(false)}
+					/>
+				) : (
+					<FontAwesomeIcon
+						icon={faBars}
+						onClick={() => setIsMenuOpen(true)}
+					/>
+				)}
+			</div>
+			<a href="/">
+				<h1 className="title">ESSENCE & EDGES</h1>
+			</a>
+			<div className="fillerDiv navIcon" />
+		</nav>
+	);
 }
